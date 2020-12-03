d2ad77b97a6ca150864d41f913cc9e230c17f4ff
aveni121 <72968622+aveni121@users.noreply.github.com>
2020-12-02 19:36:16 -0800

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -9,7 +9,7 @@ with open("collatz.txt", "w" ) as outFile:
             elif currNum % 2 != 0:
                 currNum = (currNum * 3) + 1
         if currNum == numbersToTest[-1]:
-            print("End of list has been reached. Sucess.", file = outFile)
+            print("End of list has been reached. Sucess.", file = outFile) #remove file = outFile statements from print statements to print to the console instead
             break
         if currNum == 1:
             print(numbersToTest[index], ": leads back to 1", file = outFile)
